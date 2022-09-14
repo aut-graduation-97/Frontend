85b707c31d1a8648e5757e8325fe3bee2b4a65d8
Ali MoshkinFaam <ali.fam0079@gmail.com>
2022-09-15 02:27:28 +0430

    🖌️Style: added font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+
+import Container from '@mui/material/Container';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+  return(
+    <>
+    <Container maxWidth="md">
+    <Component {...pageProps} />
+    </Container>
+
+    </>
+  ) 
 }
 
 export default MyApp
