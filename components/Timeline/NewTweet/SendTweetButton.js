6eecad0f08e889ce72945e2c7a977f2775c8ff61
Ali MoshkinFaam <ali.fam0079@gmail.com>
2022-09-18 21:36:38 +0430

    JSX: added new tweet modal

---
+++ components/Timeline/NewTweet/SendTweetButton.js
@@ -0,0 +1,22 @@
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+
+export default function SendTweetButton({ onClick, disabled, tweetContent }) {
+    
+  // For keeping modal component thinner, we can send the data to  backend from here. not a usual practice but it's ok for now
+  const sendTweetHandler = (e) => {
+    console.log(tweetContent);
+  };
+
+  return (
+    <Button
+      variant="contained"
+      endIcon={<SendIcon sx={{ mx: 1 }} />}
+      disabled={disabled ? true : false}
+      onClick={sendTweetHandler}
+      sx={{width: '50%', m:'auto'}}
+    >
+      توییت کن
+    </Button>
+  );
+}
