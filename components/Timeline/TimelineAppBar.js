1e8cc49efe179138c847809e48c1763e8123914f
Ali MoshkinFaam <ali.fam0079@gmail.com>
2022-09-19 16:13:38 +0430

    Refactor: css clean up

--- components/Timeline/TimelineAppBar.js
+++ components/Timeline/TimelineAppBar.js
@@ -1,33 +1,17 @@
-import { AppBar, Toolbar, IconButton, Box, Fab } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import MenuIcon from '@mui/icons-material/Menu';
-import AddIcon from '@mui/icons-material/Add';
+import { AppBar, Toolbar, IconButton } from '@mui/material';
+
 import HomeIcon from '@mui/icons-material/Home';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import GavelIcon from '@mui/icons-material/Gavel';
 import CollectionsIcon from '@mui/icons-material/Collections';
 import PeopleIcon from '@mui/icons-material/People';
 import Avatar from '../SharedComponents/Elements/Avatar';
+import { useRouter } from 'next/router';
 
 export default function TimelineAppBar() {
+  const router = useRouter();
   return (
     <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
       <Toolbar sx={{ display: 'flex', justifyContent: 'space-around' }}>
-        <IconButton
-          selected
-          sx={{ py: 1 }}
-          onClick={(event) => router.push('/')}
-        >
-          <HomeIcon sx={{ color: 'white' }} fontSize="large" />
-        </IconButton>
-
-        <IconButton
-          sx={{ py: 1, color: 'white' }}
-          onClick={(event) => router.push('/')}
-        >
-          <AccountCircleIcon fontSize="large" />
-        </IconButton>
-
         <IconButton
           sx={{ py: 1, color: 'white' }}
           onClick={(event) => router.push('/Mosts')}
@@ -41,11 +25,18 @@ export default function TimelineAppBar() {
         >
           <CollectionsIcon fontSize="large" />
         </IconButton>
-
+        <IconButton
+          selected
+          sx={{ py: 1 }}
+          onClick={(event) => router.push('/')}
+        >
+          <HomeIcon sx={{ color: 'white' }} fontSize="large" />
+        </IconButton>
         <IconButton onClick={(event) => router.push('/Students')}>
           <PeopleIcon sx={{ color: 'white' }} fontSize="large" />
         </IconButton>
-        <IconButton>
+
+        <IconButton sx={{ py: 1, color: 'white' }}>
           <Avatar size="32px" />
         </IconButton>
       </Toolbar>
