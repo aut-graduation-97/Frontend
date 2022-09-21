fbe9e17e432c92e437c90eba1b5fe83350ef35c7
Ali MoshkinFaam <ali.fam0079@gmail.com>
2022-09-21 21:48:45 +0430

    adding Toastify

--- components/SharedComponents/Tweets/TweetLikeButton.js
+++ components/SharedComponents/Tweets/TweetLikeButton.js
@@ -3,13 +3,24 @@ import { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import {useSession} from "next-auth/react";
+import {toast} from "react-toastify";
 
 export default function ({ likesCount }) {
   const [liked, setLiked] = useState(false);
   // FIXME: not sure if this causes bug. technically should not but react is kinda shit so ...
   const [count, setCount] = useState(likesCount);
+  const {status} = useSession();
+
+
+
 
   const likeHandler = () => {
+
+    if ( status !== 'authenticated') {
+      toast.error( 'برای لایک کردن باید وارد شوید!' );
+      return;
+    }
     // send to api
 
     // update state
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,10 @@ import createTheme from '@mui/material/styles/createTheme';
 import Container from '@mui/material/Container';
 import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
+import {ToastContainer} from "react-toastify";
+
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const theme = createTheme({
   typography: {
@@ -20,6 +24,18 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <ThemeProvider theme={theme}>
       <SessionProvider session={session}>
+        <ToastContainer
+            position="top-center"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={true}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            toastClassName={'toast'}
+        />
         <Container maxWidth="lg">
           <Component {...pageProps} />
         </Container>
