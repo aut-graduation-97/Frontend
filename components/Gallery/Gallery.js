5b855ab654799aa5c549223dd6582bb498ec4367
Ali MoshkinFaam <ali.fam0079@gmail.com>
2022-10-03 21:32:47 +0330

    Feat: gallery download button

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -1,5 +1,5 @@
 import ImageGallery from "react-image-gallery";
-import { Box, useMediaQuery } from "@mui/material";
+import { Box, Button, useMediaQuery } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import { useState } from "react";
@@ -11,7 +11,7 @@ import LoadingProgress from "../SharedComponents/UI/LoadingProgress";
 
 const DUMMY_IMAGES = [
   {
-    original: "https://picsum.photos/id/1018/1000/600/",
+    original: "https://picsum.photos/id/1018/600/1000/",
     thumbnail: "https://picsum.photos/id/1018/250/150/",
   },
   {
@@ -25,12 +25,56 @@ const DUMMY_IMAGES = [
 ];
 
 export default function Gallery({ isAdmin }) {
-  const phone = useMediaQuery("(max-width:600px)");
-
   // ONLINE - remove this return
+
+  const [selectedImageName, setSelectedImageName] = useState("");
+  const [images, setImages] = useState([]);
+  const [currentImage, setCurrentImage] = useState(0);
+  const [currentSrc, setCurrentSrc] = useState("");
+
+  // // For fetching images
+  // const { data, isLoading, error } = useQuery(["get-images"], getAllImages);
+  //
+  // // For delete
+  // const {
+  //   data: responseData,
+  //   error: responseError,
+  //   isFetching,
+  //   refetch,
+  //   isSuccess: responseIsSuccess,
+  // } = useQuery(["deleteImage"], () => deleteImage(selectedImageName), {
+  //   enabled: false,
+  // });
+  //
+  // if (responseError) toast.error(responseError.message);
+  // if (responseIsSuccess) toast.success(responseData.message);
+
+  // if (error) return <CustomError errorMessage={error.message} />;
+  // if (isLoading) return <LoadingProgress />;
+
+  //
+  // if (data && data.length !== 0) {
+  //   let images = [];
+  //   data.map((el) =>
+  //       images.push({
+  //         original: el.fullHD,
+  //         thumbnail: el.thumbnail,
+  //       })
+  //   );
+  //   setImages(images);
+  // }
+
+  const selectImageHandler = (e) => {
+    const imgSrc = e.target.src;
+    setCurrentSrc(imgSrc);
+    // extract image name from src
+    const imgName = "--extracted-image-name--";
+    setSelectedImageName(imgName);
+  };
+
   return (
     <>
-      <Box sx={{ p: 4 }}>
+      <Box sx={{ p: 4, pt: 8 }}>
         {isAdmin && (
           <LoadingButton
             color={"error"}
@@ -38,80 +82,22 @@ export default function Gallery({ isAdmin }) {
             variant="contained"
             endIcon={<DeleteForeverIcon sx={{ mr: 1 }} />}
             // onClick={() => refetch()}
-            sx={{
-              zIndex: 1000,
-              mr: 2,
-            }}
+            sx={{ zIndex: 1000, mr: 2 }}
           />
         )}
+        <Button variant={"contained"} sx={{ zIndex: 1000, mr: 2 }}>
+          <a
+            href={currentSrc}
+            download
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            دانلود تصویر
+          </a>
+        </Button>
         <Box sx={{ mt: isAdmin ? -5 : 1 }}>
-          <ImageGallery items={DUMMY_IMAGES} />
+          <ImageGallery items={DUMMY_IMAGES} onMouseOver={selectImageHandler} />
         </Box>
       </Box>
     </>
   );
-
-  const [selectedImageName, setSelectedImageName] = useState("");
-  const [images, setImages] = useState([]);
-
-  const selectImageHandler = (e) => {
-    console.log(e);
-    const imgSrc = e.target.src;
-    // extract image name from src
-    const imgName = "--extracted-image-name--";
-    setSelectedImageName(imgName);
-  };
-
-  // For fetching images
-  const { data, isLoading, error } = useQuery(["get-images"], getAllImages);
-
-  // For delete
-  const {
-    data: responseData,
-    error: responseError,
-    isFetching,
-    refetch,
-    isSuccess: responseIsSuccess,
-  } = useQuery(["deleteImage"], () => deleteImage(selectedImageName), {
-    enabled: false,
-  });
-
-  if (responseError) toast.error(responseError.message);
-  if (responseIsSuccess) toast.success(responseData.message);
-
-  if (isLoading) return <LoadingProgress />;
-  if (error) return <CustomError errorMessage={error.message} />;
-
-  if (data && data.length !== 0) {
-    let images = [];
-    data.map((el) =>
-      images.push({
-        original: el.fullHD,
-        thumbnail: el.thumbnail,
-      })
-    );
-    setImages(images);
-
-    // FIXME: need mt for mobile
-    return (
-      <>
-        <Box sx={{ p: 4 }}>
-          <LoadingButton
-            color={"error"}
-            // loading={isFetching}
-            variant="contained"
-            endIcon={<DeleteForeverIcon sx={{ mr: 1 }} />}
-            // onClick={() => refetch()}
-            sx={{
-              zIndex: 1000,
-              mr: 2,
-            }}
-          />
-          <Box sx={{ mt: -5 }}>
-            <ImageGallery items={images} onMouseOver={selectImageHandler} />
-          </Box>
-        </Box>
-      </>
-    );
-  }
 }
